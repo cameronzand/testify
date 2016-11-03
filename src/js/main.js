ca98290895e5143d25a688882a7c2cb7ff06ee7a
Cameron Zandkarimi <camcamcz@gmail.com>
2016-11-03 14:41:44 -0400

    got basic number test passed

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,23 @@
-function fizz () {
+function fizz (x) {
   // TODO
+  if (x % 3 === 0) {
+  	console.log('Fizz');
+  }
+  else if (x % 5 === 0) {
+  	console.log('Buzz');
+  }
+
+  else if (x % 15 === 0) {
+  	console.log('FizzBuzz');
+  }
+  else {
+  	return x;
+  }
 };
 
 function fizzBuzz (start, end) {
-  // Write fizzBuzz here once fizz is finished.
+  // Write fizzbuzz here once fizz is finished.
+  // for (var i = 0; i < end; i++)
 };
 
 export { fizz };
